e19e325747849a78228969039171193d544a05a3
Maureen <mlincke15@gmail.com>
2021-09-18 09:47:48 -0700

    adding API key

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@
  * You need to replace YOUR_API_KEY in the string associated with KEY with your actual API key
  */
 export const BASE_URL = 'https://api.harvardartmuseums.org';
-export const KEY = 'apikey=YOUR_API_KEY';
+export const KEY = 'apikey=5ab75450-83da-4d6e-af46-4f6c21092afe';
 
 /**
  * This will make a call to the API for a single term and value (e.g. "person", and "unknown"), and return the result
